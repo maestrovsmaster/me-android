99fb729e2f2e5da13dbe0e55198c74e5e52b5e39
Vasily Horodetskyi <vasil.horodetskyi@rational-soft.com>
2022-02-18 18:53:18 +0200

    wip

--- presentation/src/main/java/androidx/core/app/BackstackAccessor.java
+++ presentation/src/main/java/androidx/core/app/BackstackAccessor.java
@@ -1,9 +1,33 @@
 package androidx.core.app;
 
+
+
+
 import androidx.fragment.app.Fragment;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class BackstackAccessor {
     public static boolean isFragmentOnBackStack(Fragment fragment) {
         return false;
+    }
+
+    public static boolean isInBackStack(final Fragment fragment) {
+        try {
+            return false; //fragment.isInBackStack();
+        } catch (IllegalAccessError e) {
+            return isInBackStackAndroidX(fragment);
+        }
+    }
+
+    /**
+     * Hacky workaround because Fragment#isInBackStack is inaccessible with AndroidX
+     */
+    private static boolean isInBackStackAndroidX(final Fragment fragment) {
+        final StringWriter writer = new StringWriter();
+        fragment.dump("", null, new PrintWriter(writer), null);
+        final String dump = writer.toString();
+        return !dump.contains("mBackStackNesting=0");
     }
 }
