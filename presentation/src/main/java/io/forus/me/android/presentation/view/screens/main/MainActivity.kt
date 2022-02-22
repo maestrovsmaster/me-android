d47f3cf5cfc7e9f59be51acef71863e3992b7500
Vasily Horodetskyi <vasil.horodetskyi@rational-soft.com>
2022-02-22 20:13:22 +0200

    PR

--- presentation/src/main/java/io/forus/me/android/presentation/view/screens/main/MainActivity.kt
+++ presentation/src/main/java/io/forus/me/android/presentation/view/screens/main/MainActivity.kt
@@ -72,7 +72,7 @@ class MainActivity : BaseActivity() {
             } else {
                 navigateToDashboard()
                 Handler(Looper.getMainLooper()).postDelayed({
-                    startActivity(ProviderV2Activity.getCallingIntent(this@MainActivity,"0xbc0425b504457b2c071537515282dff196b8d1be", false))
+                  //  startActivity(ProviderV2Activity.getCallingIntent(this@MainActivity,"0xbc0425b504457b2c071537515282dff196b8d1be", false))
                 },500)
 
             }
